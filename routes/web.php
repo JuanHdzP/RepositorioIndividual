c94b9ad744aac31fdf3a24a161c518fed129ec65
Juan Hernandez Puebla <al222010053@gmail.com>
2021-09-20 09:24:08 -0500

    Avance hasta el dia 20-09

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/* Desde la vista principal*/
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-/* Desde la vista de una carpeta*/
-Route::get('/usuario', function () {
-    return view('usuarios/usuario');
-});
-
-/* Desde una funcion*/
-Route::get('/funcion1', function () {
-    return "Soy un Hola mundo desde una funcion";
-});
-
-/* Desde otra funcion*/
-Route::get('/funcion2', function () {
-    return "Soy un Hola mundo en otra funcion";
-});
-
-/* Ruta de blade*/
-Route::get('/hola', function () {
-    return view('child');
-});
-
